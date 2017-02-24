b86a35d1b4e426465643a334ec22cdc865f4d44e
magnus-1 <magnus-1@users.noreply.github.com>
2017-02-24 22:39:43 +0100

    tableview , shape select. training

--- paintRevolutionSwift/paintRevolutionSwift/ViewController.swift
+++ paintRevolutionSwift/paintRevolutionSwift/ViewController.swift
@@ -10,10 +10,22 @@ import Cocoa
 
 class ViewController: NSViewController {
 
+    @IBOutlet weak var selectedShape: NSTextField!
+    
+    @IBOutlet weak var shapeToolTable: NSTableView!
+    var tools = DrawTools()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        tools.add(shape: MyShape(name: "round ball"))
+        tools.add(shape: MyShape(name: "thingy"))
+        tools.add(shape: MyShape(name: "shape"))
+        shapeToolTable.delegate = self
+        shapeToolTable.dataSource = self
+        selectedShape.stringValue = "Select shape"
+        
     }
 
     override var representedObject: Any? {
@@ -21,7 +33,45 @@ class ViewController: NSViewController {
         // Update the view, if already loaded.
         }
     }
-
+    
 
 }
 
+class MyShape: ShapeToolInfo {
+    let name:String
+    init(name:String) {
+        self.name = name
+    }
+    func shapeName() -> String {
+        return name
+    }
+    
+}
+
+extension ViewController : NSTableViewDataSource {
+    func numberOfRows(in tableView: NSTableView) -> Int {
+        return tools.shapeTools.count
+    }
+}
+
+extension ViewController : NSTableViewDelegate {
+    enum ToolCellIdentifiers {
+        static let ShapeCell = "ShapeCellView"
+    }
+    
+    func tableViewSelectionDidChange(_ notification: Notification) {
+        let selectedRow = shapeToolTable.selectedRow
+        let shape = tools.shapeInfo(at: selectedRow)?.shapeName() ?? "Select shape"
+        selectedShape.stringValue = shape
+        print("tableViewSelectionDidChange: \(selectedRow) name: \(shape)")
+        
+    }
+    
+    func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
+        if let cell = tableView.make(withIdentifier: ToolCellIdentifiers.ShapeCell, owner: nil) as? ShapeToolCell {
+            cell.shapeName?.stringValue = tools.shapeInfo(at: row)?.shapeName() ?? "Shape Out of bounds"
+            return cell
+        }
+        return nil
+    }
+}
