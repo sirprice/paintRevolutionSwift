acd181ebe498accac603f41386adf8eee9bec647
magnus-1 <magnus-1@users.noreply.github.com>
2017-02-24 13:55:45 +0100

    init commit

---
+++ paintRevolutionSwift/paintRevolutionSwift/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  paintRevolutionSwift
+//
+//  Created by o_0 on 2017-02-24.
+//  Copyright © 2017 Magnus. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ paintRevolutionSwift/paintRevolutionSwift/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  paintRevolutionSwift
+//
+//  Created by o_0 on 2017-02-24.
+//  Copyright © 2017 Magnus. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
